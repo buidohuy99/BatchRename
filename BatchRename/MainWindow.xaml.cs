2194ba06542098b28378b65a014dcb20ed2485a8
sunnyhill93 <buido.huy99@gmail.com>
2019-10-27 12:20:42 +0700

    Added add files

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace BatchRename
 {
@@ -20,19 +12,21 @@ namespace BatchRename
     /// </summary>
     public partial class MainWindow : Window
     {
+
         class File
         {
-
-        }
-
-        class Folder
-        {
-
+            public string Name { get; set; }
+            public string NewName { get; set; }
+            public string Path { get; set; }
+            public string Error { get; set; }
         }
 
         public MainWindow()
         {
             InitializeComponent();
+            filesList = new BindingList<File>();
+
+            RenameFilesList.ItemsSource = filesList;
         }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
@@ -65,14 +59,44 @@ namespace BatchRename
 
         }
 
+        BindingList<File> filesList;
+
         private void AddFileButton_Click(object sender, RoutedEventArgs e)
         {
+            var dialog = new FolderBrowserDialog();
+
+            if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath + "\\";
+                var children = Directory.GetFiles(path);
+
+                foreach (string child in children)
+                {
+                    filesList.Add(new File() { Name = child.Remove(0, path.Length), Path = path });
+                }
+            }
 
         }
 
         private void ExcludeFileButton_Click(object sender, RoutedEventArgs e)
         {
+            var items = RenameFilesList.SelectedItems;
 
+            for(int item = 0; item < items.Count; item++)
+            {
+                File currentFile = items[item] as File;
+                string name = currentFile.Name;
+                string path = currentFile.Path;
+                for(int i = 0; i < filesList.Count; i++)
+                {
+                    if(filesList[i] == currentFile)
+                    {
+                        filesList.RemoveAt(i);
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
         }
 
         private void AddFolderButton_Click(object sender, RoutedEventArgs e)
