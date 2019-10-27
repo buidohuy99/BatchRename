1b94caf06bc87bed059ab6244c5d83a330161ad3
sunnyhill93 <buido.huy99@gmail.com>
2019-10-27 20:40:16 +0700

    Added exclude files, refresh button functionality and more to add files

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+using BatchRename.UtilsClass;
 
 namespace BatchRename
 {
@@ -12,26 +19,135 @@ namespace BatchRename
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<FileObj> filesList;
+        private BindingList<FolderObj> foldersList;
 
-        class File
-        {
-            public string Name { get; set; }
-            public string NewName { get; set; }
-            public string Path { get; set; }
-            public string Error { get; set; }
-        }
+        private BackgroundWorker fetchFilesWorker;
+        private BackgroundWorker excludeFilesWorker;
 
         public MainWindow()
         {
             InitializeComponent();
-            filesList = new BindingList<File>();
+            filesList = new BindingList<FileObj>();
+            foldersList = new BindingList<FolderObj>();
 
             RenameFilesList.ItemsSource = filesList;
+            RenameFoldersList.ItemsSource = foldersList;
+
+            //Create fetch files worker to invoke on click
+            fetchFilesWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+            fetchFilesWorker.DoWork += FetchFiles_DoWork;
+            fetchFilesWorker.ProgressChanged += FetchFiles_ProgressChanged;
+            fetchFilesWorker.RunWorkerCompleted += RunWorkerCompleted;
+
+            //Create exclude files worker to invoke on click
+            excludeFilesWorker = new BackgroundWorker {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true,
+            };
+            excludeFilesWorker.DoWork += ExcludeFiles_DoWork;
+            excludeFilesWorker.ProgressChanged += ExcludeFiles_ProgressChanged;
+            excludeFilesWorker.RunWorkerCompleted += RunWorkerCompleted;
+
+
+
         }
+
+        //------------------------------Background Workers---------------------------------
+
+        private void DisableLoadingViews()
+        {
+            AddFileButton.IsEnabled = false;
+            ExcludeFileButton.IsEnabled = false;
+            AddFolderButton.IsEnabled = false;
+            ExcludeFolderButton.IsEnabled = false;
+        }
+
+        private void EnableLoadingViews()
+        {
+            AddFileButton.IsEnabled = true;
+            ExcludeFileButton.IsEnabled = true;
+            AddFolderButton.IsEnabled = true;
+            ExcludeFolderButton.IsEnabled = true;
+        }
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            LoadingBar.Value = 100;
+            Mouse.OverrideCursor = null;
+
+            EnableLoadingViews();
+        }
+
+        private void FetchFiles_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            LoadingBar.Value = e.ProgressPercentage;
+
+            if(e.UserState != null)
+            {
+                filesList.Add(e.UserState as FileObj);
+            }
+        }
+
+        private void FetchFiles_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string path = (string)e.Argument + "\\";
+            var children = Directory.GetFiles(path);
+
+            for (int child = 0; child < children.Length; child++)
+            {
+                bool isDuplicated = false;
+                string childName = children[child].Remove(0, path.Length);
+
+                //Check duplicates
+                for (int i = 0; i < filesList.Count; i++)
+                {
+                    if (filesList[i].Name.Equals(childName) && filesList[i].Path.Equals(path))
+                    {
+                        isDuplicated = true;
+                        break;
+                    }
+                }
+
+                FileObj fileToAdd = null;
+                if (!isDuplicated) fileToAdd = new FileObj() { Name = childName, Path = path };
+
+                fetchFilesWorker.ReportProgress((child * 100/children.Length), fileToAdd);
+            }
+        }
+
+        private void ExcludeFiles_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            LoadingBar.Value = e.ProgressPercentage;
+        }
+
+        private void ExcludeFiles_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var items = ((IList<object>)e.Argument).Cast<FileObj>().ToList();
+
+            int amount = items.Count;
+
+            for (int item = 0; item < amount; item++)
+            {
+                Dispatcher.Invoke(()=> {
+                    filesList.Remove(items[item]);
+                });
+                excludeFilesWorker.ReportProgress((item * 100 / amount));
+            }
+        }
+
+        //-------------------------Button Clicks Implements---------------------------------
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+            LoadingBar.Value = 0;
+            filesList.Clear();
+            Mouse.OverrideCursor = null;
         }
 
         private void OptionButton_Click(object sender, RoutedEventArgs e)
@@ -59,44 +175,30 @@ namespace BatchRename
 
         }
 
-        BindingList<File> filesList;
-
         private void AddFileButton_Click(object sender, RoutedEventArgs e)
         {
+            if (fetchFilesWorker.IsBusy || excludeFilesWorker.IsBusy) return;
             var dialog = new FolderBrowserDialog();
 
             if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                string path = dialog.SelectedPath + "\\";
-                var children = Directory.GetFiles(path);
+                Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
 
-                foreach (string child in children)
-                {
-                    filesList.Add(new File() { Name = child.Remove(0, path.Length), Path = path });
-                }
+                DisableLoadingViews();
+                LoadingBar.Value = 0;
+                fetchFilesWorker.RunWorkerAsync(dialog.SelectedPath);
             }
 
         }
 
         private void ExcludeFileButton_Click(object sender, RoutedEventArgs e)
         {
-            var items = RenameFilesList.SelectedItems;
+            if (fetchFilesWorker.IsBusy || excludeFilesWorker.IsBusy || filesList.Count <= 0) return;
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
 
-            for(int item = 0; item < items.Count; item++)
-            {
-                File currentFile = items[item] as File;
-                string name = currentFile.Name;
-                string path = currentFile.Path;
-                for(int i = 0; i < filesList.Count; i++)
-                {
-                    if(filesList[i] == currentFile)
-                    {
-                        filesList.RemoveAt(i);
-                        Console.WriteLine(i);
-                        break;
-                    }
-                }
-            }
+            DisableLoadingViews();
+            LoadingBar.Value = 0;
+            excludeFilesWorker.RunWorkerAsync(RenameFilesList.SelectedItems);
         }
 
         private void AddFolderButton_Click(object sender, RoutedEventArgs e)
